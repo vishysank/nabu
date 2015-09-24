ba862199ff8934641b61e17fe8afc0f9d2d8e348
Vishwam Sankar <vishy.sankar@gmail.com>
2015-09-23 22:38:11 -0600

    routes files cleaned up a bit

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,25 @@ var pg = require('pg');
 var router = express.Router();
 var dbCalls = require('../lib/dbCalls');
 var validations = require('../lib/validations.js');
+var sql = require('../lib/pgQuery.js');
 var connectionString = process.env.DB_NAME;
 
 router.use(validations.authCheck);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var pgQuery = 'SELECT * from api_details';
 
   dbCalls.dbConnection(connectionString)
-  .then(dbCalls.dbQuery.bind(null, pgQuery, ""))
+  .then(dbCalls.dbQuery.bind(null, sql.select.apiList, ""))
   .then(function (apiList) {
     res.render('index', {
       apiList: apiList
     });
   })
-  .catch(function(err) {
-    console.log('error', err);
+  .catch(function(e){
+    console.log(e);
   });
+
 });
 
 /* GET new page. */
@@ -31,17 +32,16 @@ router.get('/new', function(req, res, next) {
 /*GET api documentation page. */
 router.get('/view/:id', function (req, res, next) {
   var id = req.params.id;
-  var pgQuery = 'SELECT * from api_auth WHERE id = $1';
 
   dbCalls.dbConnection(connectionString)
-  .then(dbCalls.dbQuery.bind(null, pgQuery, [id]))
+  .then(dbCalls.dbQuery.bind(null, sql.select.apiAuth, [id]))
   .then(function(results){
     res.render('view', {
       apiDetails: results[0]
     });
   })
-  .catch(function(err) {
-    console.log('error', err);
+  .catch(function(e){
+    console.log(e);
   });
 
 });
@@ -49,90 +49,95 @@ router.get('/view/:id', function (req, res, next) {
 /* GET edit page for specific api*/
 router.get('/edit/:id', function (req, res, next) {
   var id = req.params.id;
-  var pgQuery = 'SELECT * from api_details a, api_auth b where a.id = b.id AND a.id = $1';
 
   dbCalls.dbConnection(connectionString)
-  .then(dbCalls.dbQuery.bind(null, pgQuery, [id]))
+  .then(dbCalls.dbQuery.bind(null, sql.select.apiDetailsAndAuth, [id]))
   .then(function(results){
     res.render('edit', {apiDetails: results[0]});
   })
-  .catch(function(err) {
-    console.log('error', err);
+  .catch(function(e){
+    console.log(e);
   });
+
 });
 
 /*POST to update API details*/
 router.post('/edit/:id', function(req, res, next){
   var editAPIDetails = req.body;
   var id = req.params.id;
-  var pgQuery = 'SELECT * from api_details a, api_auth b where a.id = b.id AND a.id = $1';
-  var pgQueryAPIDetailUpdate = 'UPDATE api_details SET api_name = $2, api_desc = $3, api_owner = $4 WHERE id = $1';
-  var pgQueryAPIAuthUpdate = 'UPDATE api_auth SET api_path = $2, api_token = $3 WHERE id = $1';
   var apiDetailUpdateValues = [id, editAPIDetails.apiName, editAPIDetails.apiDesc, editAPIDetails.apiOwner];
   var apiAuthUpdateValues = [id, editAPIDetails.apiPath, editAPIDetails.apiToken];
-  var apiCheck;
 
   validations.apiCheck(editAPIDetails)
   .then(function (apiCheck) {
     if (apiCheck === 200) {
       dbCalls.dbConnection(connectionString)
-      .then(dbCalls.dbQuery.bind(null, pgQueryAPIDetailUpdate, apiDetailUpdateValues))
+      .then(dbCalls.dbQuery.bind(null, sql.update.apiDetails, apiDetailUpdateValues))
       .then(dbCalls.dbConnection.bind(null, connectionString))
-      .then(dbCalls.dbQuery.bind(null, pgQueryAPIAuthUpdate, apiAuthUpdateValues))
+      .then(dbCalls.dbQuery.bind(null, sql.update.apiAuth, apiAuthUpdateValues))
       .then(function () {
         res.redirect("/");
       })
-      .catch(function(err) {
-        console.log('error', err);
+      .catch(function(e){
+        console.log(e);
       });
     } else {
       dbCalls.dbConnection(connectionString)
-      .then(dbCalls.dbQuery.bind(null, pgQuery, [id]))
+      .then(dbCalls.dbQuery.bind(null, sql.select.apiDetailsAndAuth, [id]))
       .then(function(results){
         res.render('edit', {
           apiDetails: results[0],
-          apiError: 'Error: '+apiCheck
+          apiError: "uhhhhh...something's wrong : "+apiCheck
         });
       })
-      .catch(function(err) {
-        console.log('error', err);
+      .catch(function(e){
+        console.log(e);
       });
     }
   });
+
 });
 
-/*Post form from new page*/
+/*Post for adding a new API page*/
 router.post('/', function(req, res, next){
   var newAPIDetails = req.body;
-  var pgQueryAPIDetailInsert = 'INSERT into api_details (api_name, api_desc, api_owner, insert_datetime) values ($1, $2, $3, $4) RETURNING id';
-  var pgQueryAPIAuthInsert = 'INSERT into api_auth (id, api_path, api_token) values ($1, $2, $3)';
   var apiDetailInsertValues = [newAPIDetails.apiName,newAPIDetails.apiDesc, newAPIDetails.apiOwner,new Date()];
   var apiAuthInsertValues = ["",newAPIDetails.apiPath, newAPIDetails.apiToken];
 
-  dbCalls.dbConnection(connectionString)
-  .then(dbCalls.dbQuery.bind(null, pgQueryAPIDetailInsert, apiDetailInsertValues))
-  .then(function(results){
-    apiAuthInsertValues[0] = (results[0].id);
-    return;
+  validations.apiCheck(newAPIDetails)
+  .then(function (apiCheck) {
+    if (apiCheck === 200) {
+      dbCalls.dbConnection(connectionString)
+      .then(dbCalls.dbQuery.bind(null, sql.insert.apiDetails, apiDetailInsertValues))
+      .then(function(results){
+        apiAuthInsertValues[0] = (results[0].id);
+        return;
+      })
+      .then(dbCalls.dbConnection.bind(null, connectionString))
+      .then(dbCalls.dbQuery.bind(null, sql.insert.apiAuth, apiAuthInsertValues))
+      .then(function () {
+        res.redirect("/");
+      })
+      .catch(function(e){
+        console.log(e);
+      });
+    } else {
+      res.render('new', {
+        apiError: "uhhhhh...something's wrong : "+apiCheck,
+        apiDetails: newAPIDetails
+      });
+    }
   })
-  .then(dbCalls.dbConnection.bind(null, connectionString))
-  .then(dbCalls.dbQuery.bind(null, pgQueryAPIAuthInsert, apiAuthInsertValues))
-  .then(function () {
-    res.redirect("/");
-  })
-  .catch(function(err) {
-    console.log('error', err);
+  .catch(function(e){
+    console.log(e);
   });
 });
 
 router.get('/logout', function(req, res, next) {
   req.session.userName = null;
   req.session.password = null;
+
   res.redirect('/login');
 });
-
-
-
-
 
 module.exports = router;
