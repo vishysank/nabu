13f2b8ea18cb63fcb50bce95d7c3f3c7d3ae0d08
Vishwam Sankar <vishy.sankar@gmail.com>
2015-11-04 00:10:14 -0700

    Made the api token field optional, and added a count of number of Api's  to the Api list header

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/swagger', express.static('./node_modules/swagger-ui/dist'));
 app.use('/login', login);
