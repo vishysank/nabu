9ff8bed4f61148452744b525960b9fb0e2568675
Chris Koerber <chris.koerber@welltok.com>
2017-01-04 07:12:23 -0700

    Updated some css and made the ui a bit more user friendly

--- public/javascripts/uiRender.js
+++ public/javascripts/uiRender.js
@@ -2,7 +2,7 @@ function uiRender (apiDetails) {
   var swaggerUi = new SwaggerUi({
     url: apiDetails.api_path,
     dom_id: 'swagger-ui-container'
-  })
+  });
 
   swaggerUi.api = new SwaggerClient()
   swaggerUi.api.clientAuthorizations.authz.api_key = new SwaggerClient.ApiKeyAuthorization('Authorization', apiDetails.api_token, 'header')
